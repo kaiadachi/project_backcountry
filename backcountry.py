9abf6840050582cd6eafda9680969b82d03ba70c
kaiadachi <kaikaikai8217@gmail.com>
2019-01-12 16:33:42 +0900

    一通り ファイル作成

---
+++ backcountry.py
@@ -0,0 +1,16 @@
+# https://www.backcountry.com/
+from settings import setConst
+
+def setSelenium(target_url):
+    # options = Options()
+    # options.add_argument('--headless')
+    # self.driver = webdriver.Chrome(chrome_options=options)
+    driver = webdriver.Chrome()
+    driver.get(target_url)
+
+    return driver
+
+if __name__ == '__main__':
+    init = setConst()
+    print(init['url'])
+    #driver = setSelenium(init['url'])
---
+++ settings.py
@@ -0,0 +1,6 @@
+def setConst():
+    init = {
+        'url': 'https://www.backcountry.com/'
+    }
+
+    return init
