59f9a8636e8e8ee31c33f49e58517dde343b15b0
kaiadachi <kaikaikai8217@gmail.com>
2019-01-23 17:33:25 +0900

    done

--- parseLogic.py
+++ parseLogic.py
@@ -10,13 +10,12 @@ def parse(driver, item, df, folder_name):
     except:
         item['price'] = ''
 
-    driver.find_element_by_xpath('//span[@class = "ui-accordion-header-icon ui-icon product-details-accordion__header--inactive"]').click()
     description = driver.find_element_by_xpath('//div[@class = "ui-product-details__description"]').text
     for i in driver.find_elements_by_xpath('//li[@class = "product-details-accordion__bulletpoint"]'):
         description = description + '\n - ' + i.text
     item['description'] = description
 
-    driver.find_element_by_xpath('//span[@class = "ui-accordion-header-icon ui-icon product-details-accordion__header--inactive"]').click()
+    # driver.find_element_by_xpath('//span[@class = "ui-accordion-header-icon ui-icon product-details-accordion__header--inactive"]').click()
     name_selector = driver.find_elements_by_xpath('//div[@class = "td ui-product-details__techspec-name"]')
     value_selector = driver.find_elements_by_xpath('//div[@class = "td ui-product-details__techspec-value"]')
     for name, value in zip(name_selector,value_selector):
