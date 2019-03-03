5652572491e9b2e07954ed4aa0d1d65677ac48aa
kaiadachi <kaikaikai8217@gmail.com>
2019-03-03 15:25:19 +0900

    make singletonobj

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,7 @@ import pandas as pd
 def setConst():
     init = {
         'url': 'https://www.backcountry.com/womens-clothing-best-sellers',
+        'single_url': 'https://www.backcountry.com/womens-clothing-best-sellers',
         # 'url': 'https://www.backcountry.com/mens-clothing-best-sellers'
         # 'url': 'https://www.backcountry.com/kids-best-sellers'
         'limit': 1,
@@ -45,7 +46,12 @@ def setStructure():
         'parent_sku': '',
         'fulfillment_latency': '14',
         'merchant_shipping_group_name': 'テスト',
-        'condition_type': '新品'
+        'condition_type': '新品',
+        'bullet_point1': '',
+        'bullet_point2': '当商品は、アメリカの正規店からの並行輸入品となっていますので到着まで2-3週間お時間いただいております。',
+        'bullet_point3': '当店では完全正規品のみしか取り扱っておりませんのでご安心してご購入ください。',
+        'bullet_point4': '万が一不良品や、返品を希望される場合は当社規定のポリシーがありますので当社ページのヘルプから事前にご確認下さい。',
+        'bullet_point5': '在庫は常に変動しているため、ご注文が重なった時など在庫が確保できていない場合もございますので何卒ご了承ください。'
     }
 
     return item
