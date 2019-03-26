78911b215e8d65999a8538f75c028ba61195aee3
kai <kai@kainomacbook-es.local>
2019-03-26 12:45:45 +0700

    soleve error and change backcountry.py as a run.py

--- src/utility.py
+++ src/utility.py
@@ -20,16 +20,6 @@ def saveImg(img_url, folder_name):
 def getAttribute(selenium_array, type):
     return [i.get_attribute(type) for i in selenium_array]
 
-def getLastPage(driver):
-    last_page = driver.find_elements_by_xpath('//li[@class = "page-link page-number"]/a')[-1].text
-
-    return int(last_page)
-
-def goNext(driver, init_url):
-    driver.get('{0}?page={1}'.format(init_url, i+1))
-
-    return driver
-
 def createCsv(data, folder_csv, name, isHeader, sep):
     fliename = '{0}/{1}'.format(folder_csv, name)
     try:
