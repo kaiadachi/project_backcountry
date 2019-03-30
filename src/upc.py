1bde82f9c173fc0682e7b3b78120a92aee8d7bfd
murakami9916 <kousen_ryo9916@yahoo.co.jp>
2019-03-30 20:32:54 +0900

    get 250 item

--- src/upc.py
+++ src/upc.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def searchUpc(sku, browser):
+    browser.implicitly_wait(5)
+
     browser.get('https://upcdeal.us')
 
     input = browser.find_element_by_xpath('//div[@class = "small-10 columns"]/input')
@@ -24,11 +26,17 @@ def searchUpc(sku, browser):
         upc = browser.find_element_by_xpath('//h1').text
         upc = re.split('[ \n]', upc)[1]
     except:
-        print("Not Found UPC")
-        upc = ''
+        try:
+            upc = browser.find_element_by_xpath('//div[@class="medium-8 columns"]/h4/a').text
+            upc = re.split(' ', upc)[0]
+        except:
+            print("Not Found UPC")
+            upc = ''
+
     return upc
 
 if __name__ == '__main__':
-    item_product = 'TNF03E0-URBNV-XS'
-    upc = searchUpc(item_product)
+    item_product = 'PAT010V-BIRWH-S' # PAT010V-BIRWH-S#TNF03E0-URBNV-XS
+    browser = webdriver.Chrome('../chromedriver.exe')
+    upc = searchUpc(item_product, browser)
     print(upc)
